8cf49b7a1fffdcf1c560bdf4d373240625ed55fc
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2021-09-28 17:19:49 -0300

    montagem do servidor

---
+++ src/main.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3003;
+
+app.use(express.static(path.join(__dirname, 'src/public')));
+
+app.get('/', (req, res, next) => {
+    res.sendFile('/public/index.html');
+})
+
+app.listen(pont, () => {
+    try{
+        console.log(`Server running: http://127.0.0.1:${port}/`);
+    } catch(e) {
+        console.log(`An error has occurred.`);
+        console.log(e);
+    }
+})
