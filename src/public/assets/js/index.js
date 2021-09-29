4930a28402df6c2b986224fb6e8780a7fa2620ba
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2021-09-29 08:47:56 -0300

    separação de template

--- src/public/assets/js/index.js
+++ src/public/assets/js/index.js
@@ -1,5 +1,19 @@
 new Vue({
     el: '#app',
+    template: `
+        <div class="container mt-3">
+            <h1 class="title">{{title}}</h1>
+            <form>
+                <div class="form-group">
+                    <label for="target">Informe o texto ou URL:</label>
+                    <input class="form-control" type="text" name="target" id="target" v-model="target">
+                </div>
+            </form>
+            <div class="qrcode m-3 " v-if="target">
+                <img :src="generateQRCode" :alt="target" :title="target">
+            </div>
+        </div>
+    `,
     data: function() {
         return {
             title: 'Gerador de QRCode',
